b075f5602b84a7e02c8e70242a5e78247421ea28
YenLM25 <yendry.leon@ucr.ac.cr>
2025-06-04 11:13:01 -0600

    Creacion del proyecto

---
+++ src/main/java/ac/cr/ucr/hoVim/HoVimApplication.java
@@ -0,0 +1,13 @@
+package ac.cr.ucr.hoVim;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HoVimApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HoVimApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ac/cr/ucr/hoVim/HoVimApplicationTests.java
@@ -0,0 +1,13 @@
+package ac.cr.ucr.hoVim;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HoVimApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
