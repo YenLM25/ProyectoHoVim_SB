0b1e45fff349e074e8a6c037b786854240a8bc58
Maria Alejandra L.M <maria.leonmatarrita@ucr.ac.cr>
2025-06-06 18:06:22 -0600

    Clase Visit terminada

--- src/main/java/ac/cr/ucr/hoVim/model/Visit.java
+++ src/main/java/ac/cr/ucr/hoVim/model/Visit.java
@@ -1,4 +1,50 @@
 package ac.cr.ucr.hoVim.model;
 
 public class Visit {
+    private Integer patientId;
+    private Integer visitId;
+    private String status;
+    private String date;
+
+    public Visit() {
+    }
+
+    public Visit(Integer patientId, Integer visitId, String status, String date) {
+        this.patientId = patientId;
+        this.visitId = visitId;
+        this.status = status;
+        this.date = date;
+    }
+
+    public Integer getPatientId() {
+        return patientId;
+    }
+
+    public void setPatientId(Integer patientId) {
+        this.patientId = patientId;
+    }
+
+    public Integer getVisitId() {
+        return visitId;
+    }
+
+    public void setVisitId(Integer visitId) {
+        this.visitId = visitId;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
 }
