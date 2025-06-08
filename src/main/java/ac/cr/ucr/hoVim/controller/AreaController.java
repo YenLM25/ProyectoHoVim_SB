8eacba0190fef8b76849dd3a55fd2c48bf887b3b
DaisyMPerez <daisyperher19@gmail.com>
2025-06-08 04:01:19 -0600

    Clase AreaService

--- src/main/java/ac/cr/ucr/hoVim/controller/AreaController.java
+++ src/main/java/ac/cr/ucr/hoVim/controller/AreaController.java
@@ -1,4 +1,11 @@
 package ac.cr.ucr.hoVim.controller;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/area")
 public class AreaController {
+
+
 }
--- src/main/java/ac/cr/ucr/hoVim/service/AreaService.java
+++ src/main/java/ac/cr/ucr/hoVim/service/AreaService.java
@@ -1,4 +1,55 @@
 package ac.cr.ucr.hoVim.service;
 
-public class AreaService {
+import ac.cr.ucr.hoVim.model.Area;
+import ac.cr.ucr.hoVim.repository.AreaRegister;
+import ac.cr.ucr.hoVim.repository.IAreaRegister;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class AreaService implements IAreaRegister {
+
+    @Autowired
+    AreaRegister areaRegister;
+
+    @Override
+    public Area addArea(Area area) {
+
+        return this.areaRegister.addArea(area);
+
+    }
+
+    @Override
+    public List<Area> getAllAreas() {
+
+        return this.areaRegister.getAllAreas();
+
+    }
+
+    @Override
+    public Area getArea(Integer areaId) {
+
+        return this.areaRegister.getArea(areaId);
+
+    }
+
+    @Override
+    public Area deleteArea(Integer areaId) {
+
+        return this.areaRegister.deleteArea(areaId);
+
+    }
+
+    @Override
+    public Area editArea(Integer areaId, Area areaEdit) {
+        return this.areaRegister.editArea(areaId, areaEdit);
+    }
+
+    @Override
+    public Boolean existId(Integer areaId) {
+        return this.areaRegister.existId(areaId);
+    }
+
 }
