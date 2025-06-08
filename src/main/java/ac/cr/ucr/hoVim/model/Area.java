7b4f41e371429fa714f97232f18cfd8c3a0da8f2
DaisyMPerez <daisyperher19@gmail.com>
2025-06-08 01:30:26 -0600

    Clase Area

--- src/main/java/ac/cr/ucr/hoVim/model/Area.java
+++ src/main/java/ac/cr/ucr/hoVim/model/Area.java
@@ -1,4 +1,93 @@
 package ac.cr.ucr.hoVim.model;
 
 public class Area {
+
+    private Integer areaId;
+    private String areaName;
+    private Integer totalRooms, totalBeds;
+    private String weekdayVisitingHours, weekendVisitingHours, visitingRequirements;
+
+    public Area() {
+    }
+
+    public Area(Integer areaId, String areaName, Integer totalRooms, Integer totalBeds, String weekdayVisitingHours, String weekendVisitingHours, String visitingRequirements) {
+
+        this.areaId = areaId;
+        this.areaName = areaName;
+        this.totalRooms = totalRooms;
+        this.totalBeds = totalBeds;
+        this.weekdayVisitingHours = weekdayVisitingHours;
+        this.weekendVisitingHours = weekendVisitingHours;
+        this.visitingRequirements = visitingRequirements;
+
+    }
+
+    public Integer getAreaId() {
+        return areaId;
+    }
+
+    public void setAreaId(Integer areaId) {
+        this.areaId = areaId;
+    }
+
+    public String getAreaName() {
+        return areaName;
+    }
+
+    public void setAreaName(String areaName) {
+        this.areaName = areaName;
+    }
+
+    public Integer getTotalRooms() {
+        return totalRooms;
+    }
+
+    public void setTotalRooms(Integer totalRooms) {
+        this.totalRooms = totalRooms;
+    }
+
+    public Integer getTotalBeds() {
+        return totalBeds;
+    }
+
+    public void setTotalBeds(Integer totalBeds) {
+        this.totalBeds = totalBeds;
+    }
+
+    public String getWeekdayVisitingHours() {
+        return weekdayVisitingHours;
+    }
+
+    public void setWeekdayVisitingHours(String weekdayVisitingHours) {
+        this.weekdayVisitingHours = weekdayVisitingHours;
+    }
+
+    public String getWeekendVisitingHours() {
+        return weekendVisitingHours;
+    }
+
+    public void setWeekendVisitingHours(String weekendVisitingHours) {
+        this.weekendVisitingHours = weekendVisitingHours;
+    }
+
+    public String getVisitingRequirements() {
+        return visitingRequirements;
+    }
+
+    public void setVisitingRequirements(String visitingRequirements) {
+        this.visitingRequirements = visitingRequirements;
+    }
+
+    @Override
+    public String toString() {
+        return "Area{" +
+                "areaId=" + areaId +
+                ", areaName='" + areaName + '\'' +
+                ", totalRooms=" + totalRooms +
+                ", totalBeds=" + totalBeds +
+                ", weekdayVisitingHours='" + weekdayVisitingHours + '\'' +
+                ", weekendVisitingHours='" + weekendVisitingHours + '\'' +
+                ", visitingRequirements='" + visitingRequirements + '\'' +
+                '}';
+    }
 }
