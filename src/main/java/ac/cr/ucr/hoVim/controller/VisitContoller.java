bae295a3317c699bf27241eb0ce9e2f30dfe9fbc
YenLM25 <yendry.leon@ucr.ac.cr>
2025-06-29 14:40:47 -0600

    Error ortagrafico en la direccion mapping correjido

--- src/main/java/ac/cr/ucr/hoVim/controller/VisitContoller.java
+++ src/main/java/ac/cr/ucr/hoVim/controller/VisitContoller.java
@@ -17,7 +17,7 @@ import java.util.Optional;
 
 @CrossOrigin(origins = "http://127.0.0.1:5500")
 @RestController
-@RequestMapping("/api/hoViM/visit")
+@RequestMapping("/api/hoVim/visit")
 public class VisitContoller {
 
     @Autowired
